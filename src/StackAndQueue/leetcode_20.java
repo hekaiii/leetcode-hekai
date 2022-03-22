eca62b544ecc14f24d4339b529ca1ae23912afbe
Kai <1217073909@njupt.edu.cn>
2022-03-22 13:22:18 +0800

    0321

---
+++ src/StackAndQueue/leetcode_20.java
@@ -0,0 +1,19 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: HeKai
+ * @Date: 2022/3/22 1:16
+ */
+public class leetcode_20 {
+    public boolean isValid(String s) {
+        Stack<Character> characters = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            characters.add()
+        }
+    }
+}
