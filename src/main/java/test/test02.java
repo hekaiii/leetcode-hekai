e39be8af54fbe1c9b148d5ecb110755ff8a30eaf
Kai <hekai1616@gmail.com>
2022-04-21 22:37:12 +0800

    二叉树 前中后序遍历

--- src/main/java/test/test02.java
+++ src/main/java/test/test02.java
@@ -1,10 +1,15 @@
 package test;
 
+import javax.xml.bind.attachment.AttachmentMarshaller;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @Date: 2022/4/20 13:35
  */
 public class test02 {
     public static void main(String[] args) {
-
+        int[] a = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(a).charAt(0));
     }
 }
