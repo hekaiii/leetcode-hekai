359f34c133259bc1042cff80f181a6c933451b3c
hek32 <hekai1616@gmail.com>
2024-12-12 11:27:10 +0800

    update

--- src/main/java/BinaryTree/leetcode_222.java
+++ src/main/java/BinaryTree/leetcode_222.java
@@ -7,25 +7,45 @@ package BinaryTree;
  */
 public class leetcode_222 {
   public static int countNodes(TreeNode root) {
-    int result = 0;
-    preorder(root);
-    return result;
-  }
-
-  private static void preorder(TreeNode node) {
-    if (node == null) {
-      return;
+    if (root == null) {
+      return 0;
     }
-    preorder(node.left);
-    preorder(node.right);
+    int depthLeft = getDepth(root.left);
+    int depthRight = getDepth(root.right);
+    if (depthRight == depthLeft) {
+      //左子树满
+      return countNodes(root.right) + (1<< depthLeft);
+    } else {
+      //右子树满
+      return countNodes(root.left) + (1<< depthRight);
+    }
   }
 
+  private static int getDepth(TreeNode node) {
+    if (node == null) {
+      return 0;
+    }
+    int depth = 0;
+    while (node != null) {
+      depth++;
+      node = node.left;
+    }
+    return depth;
+  }
+
+  public static int countNodes01(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    return 1+countNodes01(root.left)+countNodes01(root.right);
+  }
   public static void main(String[] args) {
     TreeNode root = new TreeNode(1);
     root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
     root.left.left = new TreeNode(4);
-    root.left.left.left = new TreeNode(4);
-    root.left.left.left.left = new TreeNode(4);
+    root.left.right = new TreeNode(5);
+    root.right.left = new TreeNode(6);
     System.out.println(countNodes(root));
   }
 }
