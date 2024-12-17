8ab81d4543d587716960df6f4fd2aae500f5ac0c
hekai <hekai1616@gmail.com>
2024-12-17 23:08:12 +0800

    update 二叉搜索树具有如下特征： 1.节点的左子树只包含小于当前节点的数。 2.节点的右子树只包含大于当前节点的数。 3.所有左子树和右子树自身必须也是二叉搜索树。

---
+++ src/main/java/BinaryTree/leetcode_98.java
@@ -0,0 +1,34 @@
+package BinaryTree;
+
+import jdk.jfr.internal.EventWriter;
+
+/**
+ * @Author: hek32
+ * @Description:
+ * @Date: 2024/12/17
+ */
+public class leetcode_98 {
+  public static boolean isValidBST(TreeNode root) {
+    return getResult(root,null,null);
+  }
+
+  private static boolean getResult(TreeNode node, Integer min, Integer max) {
+    if (node == null) {
+      return true;
+    }
+    if ((min != null && node.val < min) || (max != null
+        && node.val > max)) {
+      return false;
+    }
+    return getResult(node.left, min, node.val) && getResult(node.right, node.val, max);
+  }
+
+  public static void main(String[] args) {
+    TreeNode node = new TreeNode(5);
+    node.left = new TreeNode(4);
+    node.right = new TreeNode(8);
+    node.right.left = new TreeNode(6);
+    node.right.right = new TreeNode(9);
+    System.out.println(isValidBST(node));
+  }
+}
