f09817a130d9c1f6589134111f41ef43256c2853
hekai <hekai1616@gmail.com>
2024-12-31 15:32:28 +0800

    update
    Signed-off-by: hek32 <hek32@chinaunicom.cn>

--- src/main/java/BackTracking/leetcode_17.java
+++ src/main/java/BackTracking/leetcode_17.java
@@ -1,6 +1,8 @@
 package BackTracking;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Author: hek32
@@ -8,11 +10,34 @@ import java.util.List;
  * @Date: 2024/12/30
  */
 public class leetcode_17 {
-//  public List<String> letterCombinations(String digits) {
-//
-//  }
+
+  ArrayList result = new ArrayList<String>();
+  ArrayList list = new ArrayList<Character>();
+  public List<String> letterCombinations(String digits) {
+    if (digits.length() == 0 || digits.length() > 4) {
+      return result;
+    }
+    String[] nums = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    char[] charArray = digits.toCharArray();
+    getResult(nums,charArray,digits,0);
+    return result;
+  }
+
+  private void getResult(String[] nums, char[] chars, String digits, int startIndex) {
+    if (list.size() == digits.length()) {
+      result.add(list.stream().map(String::valueOf).collect(Collectors.joining()));
+      return;
+    }
+    char[] array = nums[chars[startIndex] - '2'].toCharArray();
+    for (char c : array) {
+      list.add(c);
+      getResult(nums, chars, digits, startIndex + 1);
+      list.remove(list.size() - 1);
+    }
+  }
+
   public static void main(String[] args) {
-    char a = '1' + 'd';
-    System.out.println(a);
+//    List<String> result = letterCombinations("23");
+//    System.out.println(result);
   }
 }
