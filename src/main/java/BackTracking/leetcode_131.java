169ddfbc6a6b7920c8b939ebe4b80ff9930432c3
hekai <hekai1616@gmail.com>
2025-01-07 22:15:47 +0800

    update

--- src/main/java/BackTracking/leetcode_131.java
+++ src/main/java/BackTracking/leetcode_131.java
@@ -1,5 +1,6 @@
 package BackTracking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,10 +9,40 @@ import java.util.List;
  * @Date: 2025/1/2
  */
 public class leetcode_131 {
-  public List<List<String>> partition(String s) {
-    return null;
+  public static List<List<String>> partition(String s) {
+    List<String> list = new ArrayList<>();
+    List<List<String>> result = new ArrayList<>();
+    backTracking(s, 0, list, result);
+    return result;
+  }
+
+  private static void backTracking(String s, int start, List<String> list,
+      List<List<String>> result) {
+    if (start == s.length()) {
+      result.add(new ArrayList<>(list));
+      return;
+    }
+    for (int i = start; i < s.length(); i++) {
+      if (isMirror(s.substring(start, i + 1))) {
+        list.add(s.substring(start, i + 1));
+        backTracking(s, i + 1, list, result);
+        list.remove(list.size() - 1);
+      }
+    }
+  }
+
+  private static boolean isMirror(String s) {
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) != s.charAt(s.length() - 1 - i) ) {
+        return false;
+      }
+    }
+    return true;
   }
   public static void main(String[] args) {
-
+    List<List<String>> aab = partition("aaab");
+    for (List<String> strings : aab) {
+      System.out.println(strings.toString());
+    }
   }
 }
