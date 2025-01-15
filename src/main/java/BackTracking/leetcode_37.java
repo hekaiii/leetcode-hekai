c77743bb0b8d0a37458653c6feb96d7ade642083
hekai <hekai1616@gmail.com>
2025-01-15 22:47:53 +0800

    1.回溯backtracking方法一般是void返回，目的是遍历出多个答案，但如果只有一个答案，或者是只需要照打一个答案就行，此时将void-->boolean,在找到答案是return true。 同时，也就不需要回溯中一开头的if判断了，让回溯算法自然跑完所有for循环即可，如果中途没return false就找到了答案。

--- src/main/java/BackTracking/leetcode_37.java
+++ src/main/java/BackTracking/leetcode_37.java
@@ -7,28 +7,51 @@ package BackTracking;
  */
 public class leetcode_37 {
   public void solveSudoku(char[][] board) {
-    backtracking(0, 0, board);
+    backtracking(board);
   }
 
-  private boolean backtracking(int row, int column, char[][] board) {
-    for (int r = row; r <= 9; r++) {
-      for (int c = column; c <= 9; c++) {
+  private boolean backtracking(char[][] board) {
+    for (int r = 0; r <= 9; r++) {
+      for (int c = 0; c <= 9; c++) {
         if (board[r][c] != '.') {
           continue;
         }
         for (char k = '1'; k <= '9'; k++) {
           if (isValid(r, c, k, board)) {
-            board[r][c] = 'k';
-            backtracking(board);
+            board[r][c] = k;
+            if (backtracking(board)) {
+              return true;
+            }
             board[r][c] = '.';
           }
         }
+        return false;
       }
     }
+    return true;
   }
 
   private static boolean isValid(int r, int c, char k, char[][] board) {
-
+    for (int i = 0; i < 9; i++) {
+      if (board[r][i] == k) {
+        return false;
+      }
+    }
+    for (int i = 0; i < 9; i++) {
+      if (board[i][c] == k) {
+        return false;
+      }
+    }
+    int rowstart = (r / 3) * 3;
+    int columnstart = (c / 3) * 3;
+    for (int i = rowstart; i < rowstart + 3; i++) {
+      for (int j = columnstart; j < columnstart + 3; j++) {
+        if (board[i][j] == k) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
 
