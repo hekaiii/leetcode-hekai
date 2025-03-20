214fd8f274c9897a468c0ebe246a2f736da135cd
hekai <hekai1616@gmail.com>
2025-03-20 22:50:09 +0800

    update

--- src/main/java/Greedy/leetcode_406.java
+++ src/main/java/Greedy/leetcode_406.java
@@ -1,10 +1,41 @@
 package Greedy;
 
+import java.lang.management.LockInfo;
+import java.util.Arrays;
+import java.util.IdentityHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.Popup;
+
 /**
  * @Author: hekai
  * @Description:
  * @Date: 2025-03-17 21:36:08
  */
 public class leetcode_406 {
+  public static int[][] reconstructQueue(int[][] people) {
+    if (people.length == 1) {
+      return people;
+    }
+    Arrays.sort(people, (a, b) -> {
+      if (a != b) {
+        return b[0] - a[0];
+      }
+      return a[1] - b[1];
+    });
+    List<int[]> list = new LinkedList<>();
+    for (int[] person : people) {
+      list.add(person[1], person);
+    }
 
+    return list.toArray(new int[people.length][]);
+  }
+
+  public static void main(String[] args) {
+    int[][] a = new int[][]{{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+    int[][] ints = reconstructQueue(a);
+    for (int[] b : ints) {
+      System.out.println(b[1]);
+    }
+  }
 }
