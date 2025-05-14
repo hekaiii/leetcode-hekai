0b211faa6a866c06d6153560684f6143a5423d89
hekai <hekai1616@gmail.com>
2025-05-14 22:28:28 +0800

    update

---
+++ src/main/java/DynamicProgramming/leetcode_121.java
@@ -0,0 +1,40 @@
+package DynamicProgramming;
+
+/**
+ * @Author: hek32
+ * @Description:
+ * @Date: 2025/5/14
+ */
+public class leetcode_121 {
+
+  public static int maxProfit(int[] prices) {
+    //用dp思想解题 dp表示当前最大金
+    int[][] dp = new int[prices.length][2];
+    //[][0] 代表不持股 [][1] 代表持股了
+    dp[0][0] = 0;
+    dp[0][1] = -prices[0];
+    for (int i = 1; i < prices.length; i++) {
+      dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0]); //不持股 可能现在这个价格卖掉了，也可能我之前也没股
+      dp[i][1] = Math.max( - prices[i], dp[i - 1][1]); //持股 现在持股 可能我现在买股了 也可能持续持股
+    }
+    return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][1]);
+  }
+
+  //方法二
+  public static int maxProfit01(int[] prices) {
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfile = 0;
+    for (int i = 0; i < prices.length; i++) {
+      if (prices[i] < minPrice) {
+        minPrice = prices[i];
+      } else {
+        maxProfile = Math.max(maxProfile, prices[i] - minPrice);
+      }
+    }
+    return maxProfile;
+  }
+  public static void main(String[] args) {
+    int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+    System.out.println(maxProfit01(prices));
+  }
+}
