935c3068ef7b90eb5b1e11afd7c5379c2363f13f
hekai <hekai1616@gmail.com>
2025-05-19 23:32:57 +0800

    update

--- src/main/java/DynamicProgramming/leetcode_714.java
+++ src/main/java/DynamicProgramming/leetcode_714.java
@@ -24,7 +24,7 @@ public class leetcode_714 {
   public static int maxProfit(int[] prices, int fee) {
     int[][] dp = new int[prices.length][2];
     dp[0][0] = 0;
-    //手上有一只股票
+    //手上有一只股票 只有两个状态
     dp[0][1] = -prices[0];
     for (int i = 1; i < prices.length; i++) {
       dp[i][0] = Math.max(dp[i - 1][1] + prices[i] - fee, dp[i - 1][0]);
