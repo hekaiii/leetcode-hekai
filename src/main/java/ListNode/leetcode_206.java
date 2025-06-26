9c4dc45ec070cd7e7b55f984fd697ce93cabc47d
hekai <hekai1616@gmail.com>
2025-06-26 23:31:39 +0800

    2025.6.26 复习回顾

--- src/main/java/ListNode/leetcode_206.java
+++ src/main/java/ListNode/leetcode_206.java
@@ -11,83 +11,20 @@ import java.util.Stack;
  */
 public class leetcode_206 {
 
-    public ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
         ListNode pre = null;
-        ListNode temp = null;
-        ListNode cur = head;
-        while (cur != null) {
-            temp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = temp;
+        while (head != null) {
+            ListNode tmp = head.next; //1.保存下一步
+            head.next = pre; //2.当前步可以掉头了
+            pre = head; //3.后一步站当前步
+            head = tmp; //4.当前步前进一步
         }
         return pre;
     }
 
-    public ListNode reverseList01(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode pre = null;
-        ListNode temp = null;
-        ListNode cur = head;
-        while (cur != null) {
-            temp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = temp;
-        }
-        return pre;
-    }
-
-    public static ListNode reverseList02(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        Stack<Integer> stack = new Stack<>();
-        ListNode cur = head;
-        while (cur != null) {
-            stack.push(cur.val);
-            cur = cur.next;
-        }
-        ListNode result = new ListNode();
-        ListNode cursor = result;
-        while (!stack.empty()) {
-            cursor.next = new ListNode(stack.pop());
-            cursor = cursor.next;
-        }
-        return result.next;
-    }
-
-    public static ListNode reverseList03(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode pre = null;
-        ListNode temp = null;
-        ListNode cursor = head;
-        while (cursor != null) {
-            temp = cursor.next;
-            cursor.next = pre;
-            pre = cursor;
-            cursor = temp;
-        }
-        return pre;
-    }
-    public static ListNode reverseList04(ListNode head) {
-        ListNode slow = null;
-        ListNode fast = head;
-        while (fast != null) {
-            ListNode tmp = fast;
-            fast = fast.next;
-            tmp.next = slow;
-            slow = tmp;
-        }
-        return slow;
-    }
     public static void main(String[] args) {
         ListNode listNode = ListNode.init(5);
-        ListNode listNode1 = reverseList04(listNode);
+        ListNode listNode1 = reverseList(listNode);
         ListNode.print(listNode1);
     }
 
--- src/main/java/ListNode/leetcode_234.java
+++ src/main/java/ListNode/leetcode_234.java
@@ -2,6 +2,8 @@ package ListNode;
 
 import com.sun.org.apache.xerces.internal.impl.xpath.regex.REUtil;
 
+import java.util.List;
+
 /**
  * @author hekai
  * @date 2025/6/23 20:33
@@ -9,7 +11,33 @@ import com.sun.org.apache.xerces.internal.impl.xpath.regex.REUtil;
  */
 public class leetcode_234 {
     public boolean isPalindrome(ListNode head) {
-        return false;
+        if (head == null) {
+            return true;
+        }
+        int count = 1;
+        ListNode dummy = head;
+        while (dummy.next != null) {
+            count++;
+            dummy = dummy.next;
+        }
+        int[] list = new int[count];
+        list[0] = head.val;
+        dummy = head;
+        int a = 1;
+        while (dummy.next != null) {
+            list[a++] = dummy.next.val;
+            dummy = dummy.next;
+        }
+        for (int i = 0; i < list.length / 2; i++) {
+            if (list[i] != list[list.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    //快慢指针+反转链表
+    public boolean isPalindrome01(ListNode head) {
+
     }
     public static void main(String[] args) {
 
